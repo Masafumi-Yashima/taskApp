e8b6fd7deb9b2711ec0d37490d5c9a6b8cb700dd
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-02-02 23:23:37 +0900

    タスク作成/編集画面でタスクの情報を反映、保存させるの完了

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -7,20 +7,45 @@
 //
 
 import UIKit
+import RealmSwift
 
 class InputViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var dataPicker: UIDatePicker!
     
+    let realm = try! Realm()
     var task: Task!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //背景をタップしたらdismissKyeboardメソッドを呼ぶように設定する
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKyeboard))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        dataPicker.date = task.date
+        
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.dataPicker.date
+            self.realm.add(self.task, update: .modified)
+        }
+        
+        super.viewWillDisappear(animated)
+    }
+        
+    @objc func dismissKyeboard() {
+        //キーボードを閉じる
+        view.endEditing(true)
+    }
 
     /*
     // MARK: - Navigation
